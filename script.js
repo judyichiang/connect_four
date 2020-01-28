4f62df0d30299bf51254ec0131338cca30964093
Keith Tachibana <Keith.Tachibana@gmail.com>
2020-01-28 15:31:28 -0800

    Added animations, reset button, and other CSS styling

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ const button5 = document.querySelector('.col-5');
 const button6 = document.querySelector('.col-6');
 const gameInfo = document.getElementById('gameInfo');
 const start = document.getElementById('startGame');
+const modal = document.querySelector('.modal');
+const close = document.querySelector('.close');
+const restart = document.querySelector('.restart');
+const congrats = document.querySelector('.congrats');
+const reset = document.getElementById('resetGame');
+const p1Wins = document.getElementById('p1Wins');
+const p2Wins = document.getElementById('p2Wins');
 
 var gameActive = false;
 var activePlayer = 1;
@@ -15,6 +22,8 @@ var playerColor = [];
 playerColor[1] = "red";
 playerColor[2] = "blue";
 var col, row;
+var p1GamesWon = 0;
+var p2GamesWon = 0;
 
 button0.addEventListener('click', function() {
   drop(0);
@@ -38,7 +47,14 @@ button6.addEventListener('click', function () {
   drop(6);
 });
 start.addEventListener('click', startGame);
-
+close.addEventListener('click', function() {
+  modal.classList.add('hidden');
+})
+restart.addEventListener('click', function() {
+  modal.classList.add('hidden');
+  startGame();
+})
+reset.addEventListener('click', resetGame);
 
 function startGame() {
   if (gameActive === true) {
@@ -52,11 +68,43 @@ function startGame() {
       gameBoard[row][col] = 0;
     }
   }
+  gameInfo.innerHTML = '';
   for (var i = 0; i < 7; i++) {
-    document.querySelector('.col-' + i).disabled = false;
+    document.querySelector('.col-' + i).classList.remove('hidden');
   }
+  for (row = 0; row <= 5; row++) {
+    for (col = 0; col <= 6; col++) {
+      var initDisc = document.getElementById('td'+row +col);
+      initDisc.classList.remove('fall-'+row);
+    }
+  }
+  start.setAttribute('disabled', 'true');
   updateBoard();
-  updateTurn();
+}
+
+function resetGame() {
+  p1GamesWon = 0;
+  p2GamesWon = 0;
+  p1Wins.textContent = "Player 1 Number of Wins: " + p1GamesWon;
+  p2Wins.textContent = "Player 2 Number of Wins: " + p2GamesWon;
+  gameInfo.innerHTML = '';
+  for (row = 0; row <= 5; row++) {
+    gameBoard[row] = [];
+    for (col = 0; col <= 6; col++) {
+      gameBoard[row][col] = 0;
+    }
+  }
+  for (row = 0; row <= 5; row++) {
+    for (col = 0; col <= 6; col++) {
+      var initDisc = document.getElementById('td' + row + col);
+      initDisc.classList.remove('fall-' + row);
+    }
+  }
+  for (col = 0; col <= 6; col++) {
+    for (row = 0; row <= 5; row++) {
+      document.getElementById('td' + row + col).innerHTML = "<span class='tdElement player" + gameBoard[row][col] + "'> </span>";
+    }
+  }
 }
 
 function updateBoard() {
@@ -69,6 +117,10 @@ function updateBoard() {
 }
 
 function updateTurn() {
+  var p1WinsInfo = "Player 1 Number of Wins: " + p1GamesWon;
+  var p2WinsInfo = "Player 2 Number of Wins: " + p2GamesWon;
+  p1Wins.textContent = p1WinsInfo;
+  p2Wins.textContent = p2WinsInfo;
   if (gameActive) {
     gameInfo.innerHTML = "Current Player: Player " + activePlayer + " <span class='player" + activePlayer + "'>(" + playerColor[activePlayer] + ")</span>";
   }
@@ -82,6 +134,11 @@ function checkWin() {
         if (gameBoard[row][col] === i) {
           if ((gameBoard[row][col+1] === i) && (gameBoard[row][col+2] === i) && (gameBoard[row][col+3] === i)) {
             endGame(i);
+            if (i === 1) {
+              p1GamesWon += 1;
+            } else {
+              p2GamesWon += 1;
+            }
             return true;
           }
         }
@@ -89,12 +146,17 @@ function checkWin() {
     }
   }
   // Check top to bottom
-  for (var i = 1; i <= 2; i++) {
+  for (i = 1; i <= 2; i++) {
     for (col = 0; col <= 6; col++) {
       for (row = 0; row <= 2; row++) {
         if (gameBoard[row][col] === i) {
           if ((gameBoard[row+1][col] === i) && (gameBoard[row+2][col] === i) && (gameBoard[row+3][col] === i)) {
             endGame(i);
+            if (i === 1) {
+              p1GamesWon += 1;
+            } else {
+              p2GamesWon += 1;
+            }
             return true;
           }
         }
@@ -102,12 +164,17 @@ function checkWin() {
     }
   }
   // Check down diagonally
-  for (var i = 1; i <= 2; i++) {
+  for (i = 1; i <= 2; i++) {
     for (col = 0; col <= 3; col++) {
       for (row = 0; row <= 2; row++) {
         if (gameBoard[row][col] === i) {
           if ((gameBoard[row+1][col+1] === i) && (gameBoard[row+2][col+2] === i) && (gameBoard[row+3][col+3] === i)) {
             endGame(i);
+            if (i === 1) {
+              p1GamesWon += 1;
+            } else {
+              p2GamesWon += 1;
+            }
             return true;
           }
         }
@@ -115,12 +182,17 @@ function checkWin() {
     }
   }
   // Check up diagonally
-  for (var i = 1; i <= 2; i++) {
+  for (i = 1; i <= 2; i++) {
     for (col = 0; col <= 3; col++) {
       for (row = 3; row <= 5; row++) {
         if (gameBoard[row][col] === i) {
           if ((gameBoard[row-1][col+1] === i) && (gameBoard[row-2][col+2] === i) && (gameBoard[row-3][col+3] === i)) {
             endGame(i);
+            if (i === 1) {
+              p1GamesWon += 1;
+            } else {
+              p2GamesWon += 1;
+            }
             return true;
           }
         }
@@ -133,14 +205,19 @@ function endGame(winner) {
   gameActive = false;
   gameInfo.textContent = "Winner: " + winner;
   for (var i = 0; i < 7; i++) {
-    document.querySelector('.col-'+i).disabled = true;
+    document.querySelector('.col-'+i).classList.add('hidden');
   }
+  start.removeAttribute('disabled');
+  modal.classList.remove('hidden');
+  congrats.textContent = "Congratulations player " + activePlayer + ", you have won!";
 }
 
 function drop(col) {
   for (row = 5; row >= 0; row--) {
     if (gameBoard[row][col] === 0) {
       gameBoard[row][col] = activePlayer;
+      var disc = document.getElementById('td'+row+col);
+      disc.classList.add('fall-'+row);
       updateBoard();
       activePlayer = (activePlayer === 1) ? 2 : 1;
       updateTurn();
