4c4a236745acb27f2f3b774b771ba5d49ee9b3dc
Judy Chiang <jiyc1119@gmail.com>
2020-01-28 16:20:22 -0800

    refactored volume-mute

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ const reset = document.getElementById('resetGame');
 const p1Wins = document.getElementById('p1Wins');
 const p2Wins = document.getElementById('p2Wins');
 const playTheme = document.getElementById('theme');
+const mute = document.getElementById('music');
 
 var gameActive = false;
 var activePlayer = 1;
@@ -26,7 +27,7 @@ var col, row;
 var p1GamesWon = 0;
 var p2GamesWon = 0;
 
-button0.addEventListener('click', function() {
+button0.addEventListener('click', function () {
   drop(0);
 });
 button1.addEventListener('click', function () {
@@ -48,10 +49,10 @@ button6.addEventListener('click', function () {
   drop(6);
 });
 start.addEventListener('click', startGame);
-close.addEventListener('click', function() {
+close.addEventListener('click', function () {
   modal.classList.add('hidden');
 })
-restart.addEventListener('click', function() {
+restart.addEventListener('click', function () {
   modal.classList.add('hidden');
   startGame();
 })
@@ -75,12 +76,13 @@ function startGame() {
   }
   for (row = 0; row <= 5; row++) {
     for (col = 0; col <= 6; col++) {
-      var initDisc = document.getElementById('td'+row +col);
-      initDisc.classList.remove('fall-'+row);
+      var initDisc = document.getElementById('td' + row + col);
+      initDisc.classList.remove('fall-' + row);
     }
   }
   start.setAttribute('disabled', 'true');
   updateBoard();
+  playAudio();
 }
 
 function resetGame() {
@@ -107,7 +109,6 @@ function resetGame() {
     }
   }
   updateTurn();
-  playAudio();
 }
 
 
@@ -134,13 +135,23 @@ function playAudio() {
   playTheme.play();
 }
 
+mute.addEventListener('click', function () {
+  if (playTheme.muted === false) {
+    playTheme.muted = true;
+  }
+  else{
+    playTheme.muted = false;
+  }
+});
+
+
 function checkWin() {
   // Check left to right
   for (var i = 1; i <= 2; i++) {
     for (col = 0; col <= 3; col++) {
       for (row = 0; row <= 5; row++) {
         if (gameBoard[row][col] === i) {
-          if ((gameBoard[row][col+1] === i) && (gameBoard[row][col+2] === i) && (gameBoard[row][col+3] === i)) {
+          if ((gameBoard[row][col + 1] === i) && (gameBoard[row][col + 2] === i) && (gameBoard[row][col + 3] === i)) {
             endGame(i);
             if (i === 1) {
               p1GamesWon += 1;
@@ -158,7 +169,7 @@ function checkWin() {
     for (col = 0; col <= 6; col++) {
       for (row = 0; row <= 2; row++) {
         if (gameBoard[row][col] === i) {
-          if ((gameBoard[row+1][col] === i) && (gameBoard[row+2][col] === i) && (gameBoard[row+3][col] === i)) {
+          if ((gameBoard[row + 1][col] === i) && (gameBoard[row + 2][col] === i) && (gameBoard[row + 3][col] === i)) {
             endGame(i);
             if (i === 1) {
               p1GamesWon += 1;
@@ -176,7 +187,7 @@ function checkWin() {
     for (col = 0; col <= 3; col++) {
       for (row = 0; row <= 2; row++) {
         if (gameBoard[row][col] === i) {
-          if ((gameBoard[row+1][col+1] === i) && (gameBoard[row+2][col+2] === i) && (gameBoard[row+3][col+3] === i)) {
+          if ((gameBoard[row + 1][col + 1] === i) && (gameBoard[row + 2][col + 2] === i) && (gameBoard[row + 3][col + 3] === i)) {
             endGame(i);
             if (i === 1) {
               p1GamesWon += 1;
@@ -194,7 +205,7 @@ function checkWin() {
     for (col = 0; col <= 3; col++) {
       for (row = 3; row <= 5; row++) {
         if (gameBoard[row][col] === i) {
-          if ((gameBoard[row-1][col+1] === i) && (gameBoard[row-2][col+2] === i) && (gameBoard[row-3][col+3] === i)) {
+          if ((gameBoard[row - 1][col + 1] === i) && (gameBoard[row - 2][col + 2] === i) && (gameBoard[row - 3][col + 3] === i)) {
             endGame(i);
             if (i === 1) {
               p1GamesWon += 1;
@@ -213,7 +224,7 @@ function endGame(winner) {
   gameActive = false;
   gameInfo.textContent = "Winner: " + winner;
   for (var i = 0; i < 7; i++) {
-    document.querySelector('.col-'+i).classList.add('hidden');
+    document.querySelector('.col-' + i).classList.add('hidden');
   }
   start.removeAttribute('disabled');
   modal.classList.remove('hidden');
@@ -224,8 +235,8 @@ function drop(col) {
   for (row = 5; row >= 0; row--) {
     if (gameBoard[row][col] === 0) {
       gameBoard[row][col] = activePlayer;
-      var disc = document.getElementById('td'+row+col);
-      disc.classList.add('fall-'+row);
+      var disc = document.getElementById('td' + row + col);
+      disc.classList.add('fall-' + row);
       updateBoard();
       activePlayer = (activePlayer === 1) ? 2 : 1;
       updateTurn();
